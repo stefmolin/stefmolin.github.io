8bb49b2b6426aabbdd3ce78ab3b399bc68092b13
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-01-20 16:16:27 -0500

    Add sitemap and robots.txt autogeneration.

---
+++ app/next-sitemap.config.js
@@ -0,0 +1,67 @@
+const now = new Date().toISOString();
+
+const PRIORITIES = {
+  newsPage: 0.1,
+  feedPage: 0.5,
+  blogPost: 0.6,
+  workshopsPage: 0.7,
+  booksPage: 0.8,
+  homePage: 0.9,
+  articlePost: 1.0,
+};
+
+const PAGE_CONFIGS = [
+  {
+    regex: /^\/articles\/\w+/,
+    priority: PRIORITIES.articlePost,
+    changefreq: "yearly",
+  },
+  {
+    regex: /^\/blog\/\w+/,
+    priority: PRIORITIES.blogPost,
+    changefreq: "yearly",
+  },
+  {
+    regex: /^\/(articles|blog)$/,
+    priority: PRIORITIES.feedPage,
+    changefreq: "monthly",
+  },
+];
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://stefaniemolin.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false, // not enough pages to warrant this
+  priority: 0.5, // default priority
+  transform: async (config, path) => {
+    let pageChangefreq;
+    let pagePriority;
+
+    for (const { regex, priority, changefreq } of PAGE_CONFIGS) {
+      if (regex.exec(path)) {
+        pagePriority = priority;
+        pageChangefreq = changefreq;
+        break;
+      }
+    }
+
+    // Use default transformation for all other cases
+    return {
+      loc: path,
+      changefreq: pageChangefreq ?? config.changefreq,
+      priority: pagePriority ?? config.priority,
+      lastmod: now,
+    };
+  },
+
+  // TODO: remove this once the old homepage is no longer needed
+  additionalPaths: async (config) => [
+    {
+      loc: "/",
+      changefreq: "weekly",
+      priority: PRIORITIES.homePage,
+      lastmod: now,
+    },
+  ],
+};
