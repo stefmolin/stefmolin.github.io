4c8a572f50f31e25e2a28e096ed6834bceb6ba2d
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-01-26 18:41:53 -0500

    Address issues

--- app/components/date-formatter.tsx
+++ app/components/date-formatter.tsx
@@ -1,23 +1,21 @@
-import { ReactElement } from "react";
-import { DateTime } from "luxon";
+import { ReactNode } from 'react';
+import { DateTime } from 'luxon';
 
 type Props = {
   dateString: string;
   relative?: boolean;
   long?: boolean;
-  children?: ReactElement[];
+  children?: ReactNode[];
 };
 
 const DateFormatter = ({ dateString, relative, long, children }: Props) => {
   const date = DateTime.fromISO(dateString);
   return (
     <time dateTime={dateString}>
-      {...children}
+      {...children ?? []}
       {date > DateTime.now().minus({ days: 2 }) && relative
         ? date.toRelative()
-        : date.toLocaleString(
-            long ? DateTime.DATETIME_FULL : DateTime.DATE_FULL
-          )}
+        : date.toLocaleString(long ? DateTime.DATETIME_FULL : DateTime.DATE_FULL)}
     </time>
   );
 };
