e4229ff3c5f87e657ee5f51f33ec953ed4b37e54
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-01-27 14:53:43 -0500

    Update newsletter subscribe button layout

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCode,
   faCoffee,
-  faEnvelopeOpen,
   faHeart,
   faLock,
   faRssSquare,
@@ -13,143 +12,10 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import Avatar from './avatar';
 import SubscribeToNewsletterForm from './subscribe-to-newsletter';
-import SubscribeButtons from './subscribe';
 import FollowButtons from './follow';
-import { NEWSLETTER_URL } from '../lib/constants';
-
-// const Footer = () => {
-//   return (
-//     <footer className="bg-neutral-50 border-t border-neutral-200">
-//       <Container>
-//         <div className="py-5 flex flex-col lg:flex-row items-center">
-//           <h3 className="text-center lg:text-left lg:pr-4 lg:w-1/2">
-//             Made with <FontAwesomeIcon icon={faCoffee} />,{" "}
-//             <FontAwesomeIcon icon={faCode} />, and lots of{" "}
-//             <FontAwesomeIcon icon={faHeart} />.
-//           </h3>
-//           <div className="justify-center lg:justify-end text-center lg:text-right lg:pl-4 lg:w-1/2">
-//             &#169; 2019&ndash;{DateTime.now().year} Stefanie Molin
-//           </div>
-//         </div>
-//       </Container>
-//     </footer>
-//   );
-// };
-
-// const linkProps = {
-//   className: "text-slate-600 hover:text-slate-800",
-//   target: "_blank",
-//   rel: "noopener noreferrer",
-// };
-
-// // TODO: take into account the page when determining what to show in the footer (tipping only on articles for example)
-
-// const Footer = () => {
-//   return (
-//     <footer className="bg-neutral-50 border-t border-neutral-200">
-//       <Container>
-//         <div className="mt-5 flex flex-col lg:flex-row items-center">
-//           <div className="text-left lg:px-4 lg:w-1/2">
-//             <Avatar name="Stefanie Molin" picture="/assets/avatar.jpeg" />
-//             <p className="pt-5">
-//               Thanks for reading! I am passionate about teaching data science
-//               and software engineering skills to people of all levels. I have
-//               created multiple workshops, books, and articles, as well as
-//               contributed to various FOSS projects. If this or any of my other
-//               content has helped you, please consider{" "}
-//               <a
-//                 className="text-slate-600 hover:text-slate-800 underline"
-//                 href="https://www.buymeacoffee.com/stefanie.molin"
-//                 target="_blank"
-//                 rel="noopener noreferrer">
-//                 supporting
-//               </a>{" "}
-//               me.
-//             </p>
-//           </div>
-//           <div className="hidden lg:flex flex-row  items-start justify-evenly text-left lg:pl-4 lg:w-1/2">
-//             {/* <SubscribeToNewsletterForm /> */}
-//             {/* <div className="items-end space-x-2 mt-26">
-//               <span>Don't forget to subscribe: </span>
-//               <SubscribeButtons />
-//             </div> */}
-//             {/* <div className="flex flex-col">
-//               <span>Follow Me</span>
-//               <FollowButtons />
-//             </div> */}
-//             <div className="flex flex-col">
-//               <div>
-//                 <h6 className="font-bold text-lg text-slate-800">Subscribe</h6>
-//               </div>
-//               <div>
-//                 <a href={NEWSLETTER_URL} {...linkProps}>
-//                   <FontAwesomeIcon icon={faEnvelopeOpen} /> Newsletter{" "}
-//                 </a>
-//               </div>
-//               <div>
-//                 <a href="/feeds/articles-rss.xml" {...linkProps}>
-//                   <FontAwesomeIcon icon={faRssSquare} className="pr-0.5" />{" "}
-//                   Article Feed{" "}
-//                 </a>
-//               </div>
-//               <div>
-//                 <a href="/feeds/blog-rss.xml" {...linkProps}>
-//                   <FontAwesomeIcon icon={faRssSquare} className="pr-0.5" /> Blog
-//                   Feed{" "}
-//                 </a>
-//               </div>
-//             </div>
-//             <div className="flex flex-col">
-//               <div>
-//                 <h6 className="font-bold text-lg text-slate-800">
-//                   Suggested Links
-//                 </h6>
-//               </div>
-//               <div>
-//                 <a href="" {...linkProps}>
-//                   About the Author
-//                 </a>
-//               </div>
-//               <div>
-//                 <a href="" {...linkProps}>
-//                   Interviews
-//                 </a>
-//               </div>
-//               {/* <div>
-//                 <a href="" {...linkProps}>
-//                   News and Events
-//                 </a>
-//               </div> */}
-//             </div>
-//           </div>
-//         </div>
-//         {/* <div className="py-5 flex flex-col lg:flex-row items-center">
-//           <div className="lg:pl-4 lg:w-1/2">
-//             &#169; 2019&ndash;{DateTime.now().year} Stefanie Molin
-//           </div>
-//           <div className="justify-center lg:justify-end text-center lg:text-right lg:pl-4 lg:w-1/2">
-//             All opinions are my own.
-//           </div> */}
-//         {/* <h3 className="text-center lg:text-left lg:pr-4 lg:w-1/2">
-//             <div>Don't forget to subscribe: </div>
-//             <SubscribeButtons />
-//           </h3> */}
-//         <div className="py-5 text-center">
-//           &#169; 2019&ndash;{DateTime.now().year} Stefanie Molin
-//         </div>
-//         {/* </div> */}
-//       </Container>
-//     </footer>
-//   );
-// };
+import Link from 'next/link';
 
 const SitemapLinks = ({ className }: { className?: string }) => {
-  const linkProps = {
-    className: classNames(className, 'font-bold text-slate-500 hover:text-slate-800'),
-    target: '_blank',
-    rel: 'noopener noreferrer',
-  };
-
   // TODO: probably want a way to group by content and keep those together in the column
   // since this fills rows and then columns things that are related can end up far apart when they
   // should be in the same column
@@ -168,58 +34,70 @@ const SitemapLinks = ({ className }: { className?: string }) => {
     { name: 'News', url: '/news' },
     { name: 'Events', url: '/events' },
     { name: 'Contact', url: '/contact' },
+    // { name: 'Buy Me a Coffee', url: 'https://www.buymeacoffee.com/stefanie.molin' },
   ];
   return (
     <>
       {...siteLinks.map(({ name, url }) => (
-        <a key={url} href={url} {...linkProps}>
+        // <a key={url} href={url} {...linkProps}>
+        //   {name}
+        // </a>
+        <Link
+          href={url}
+          className={classNames(className, 'font-bold text-slate-500 hover:text-slate-800')}
+        >
           {name}
-        </a>
+        </Link>
       ))}
     </>
   );
 };
 
-// TODO: take into account the page when determining what to show in the footer (tipping only on articles for example)
+// TODO: take into account the page when determining what to show in the footer
+// (tipping only on articles for example)
 
 const FooterLinks = ({ className }: { className?: string }) => {
   const linkProps = {
     className: 'text-slate-600 hover:text-slate-800',
-    target: '_blank',
-    rel: 'noopener noreferrer',
+    // target: '_blank',
+    // rel: 'noopener noreferrer',
   };
   return (
     <div className={classNames(className, 'space-x-2 items-center')}>
       {/* Follow <FontAwesomeIcon icon={faUserPlus} /> */}
-      <a href="/feeds/articles-rss.xml" {...linkProps}>
+      <Link href="/feeds/articles-rss.xml" {...linkProps}>
         <FontAwesomeIcon icon={faRssSquare} fixedWidth /> Article Feed{' '}
-      </a>
-      <a href="/feeds/blog-rss.xml" {...linkProps}>
+      </Link>
+      <Link href="/feeds/blog-rss.xml" {...linkProps}>
         <FontAwesomeIcon icon={faRssSquare} fixedWidth /> Blog Feed{' '}
-      </a>
-      <a href="/privacy-policy" {...linkProps}>
+      </Link>
+      <Link href="/privacy-policy" {...linkProps}>
         <FontAwesomeIcon icon={faLock} fixedWidth /> Privacy Policy{' '}
-      </a>
-      <a href="/sitemap.xml" {...linkProps}>
+      </Link>
+      <Link href="/sitemap.xml" {...linkProps}>
         <FontAwesomeIcon icon={faSitemap} fixedWidth /> Sitemap{' '}
-      </a>
+      </Link>
     </div>
   );
 };
+
+// TODO: on the privacy page, it doesn't really make sense to have the blurb about me
+// or the newsletter signup really.., maybe allow that to be turned off on demand?
+// or at least the text overwritten?
 
 const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200 mx-auto px-10">
       <div className="mt-5 flex flex-col lg:flex-row items-start">
-        <div className="text-left lg:px-4 lg:w-1/2">
+        <div className="text-left lg:pl-4 lg:pr-10 lg:w-2/3 xl:mr-20">
           <Avatar name="Stefanie Molin" picture="/assets/avatar.jpeg" />
           <p className="pt-5">
-            Thanks for reading! I am passionate about teaching data science and software engineering
-            skills to people of all levels. I have created multiple workshops, books, and articles,
-            as well as contributed to various FOSS projects. If this or any of my other content has
-            helped you, please consider{' '}
+            Thanks for visiting my website! I am passionate about teaching data science and software
+            engineering skills to people of all levels. I have created multiple workshops, books,
+            and articles, as well as contributed to various FOSS projects. If any of my other
+            content has helped you, please consider{' '}
             <a
-              className="text-slate-600 hover:text-slate-800 underline"
+              className="text-slate-500 hover:text-slate-800 underline"
               href="https://www.buymeacoffee.com/stefanie.molin"
               target="_blank"
               rel="noopener noreferrer"
@@ -229,30 +107,17 @@ const Footer = () => {
             me.
           </p>
         </div>
-        <div className="lg:flex flex-row items-start justify-end text-left lg:pl-4 lg:w-1/2 ml-20 mr-5">
+
+        <hr className="lg:hidden my-8 w-full" />
+        <div className="px-20 md:px-44 lg:px-0 w-full lg:w-1/3 flex flex-row items-start content-center lg:justify-end xl:ml-20 lg:mr-5">
           <SubscribeToNewsletterForm />
         </div>
       </div>
-
-      {/* <div className="flex flex-wrap items-center justify-evenly text-center pt-10 px-4">
-        <SitemapLinks className="px-5" />
-      </div>
-
-      <hr /> */}
+      <hr className="lg:hidden mt-8 w-full" />
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 text-center pt-10 px-4">
         <SitemapLinks className="px-5" />
       </div>
-
-      {/* <div className="inline-flex items-center w-full">
-        <hr className="w-1/2 my-8 grow" />
-        <div className="inline-flex justify-end w-full">
-          <hr className="w-1/2 my-8" />
-        </div>
-        <span className="absolute px-3 text-gray-900 -translate-x-1/2 left-1/2">
-          <FollowButtons size="3x" />
-        </span>
-      </div> */}
 
       <div className="flex flex-row items-center justify-center pt-10">
         <hr className="my-8 w-full" />
@@ -260,19 +125,19 @@ const Footer = () => {
         <hr className="my-8 w-full" />
       </div>
 
-      <div className="flex flex-col lg:flex-row items-center">
-        <div className="lg:pl-4 lg:w-1/2">All opinions are my own.</div>
-        <div className="justify-center lg:justify-end text-center lg:text-right lg:pl-4 lg:w-1/2">
+      <div className="flex flex-col lg:flex-row items-center lg:px-2">
+        <div className="lg:w-1/2">All opinions are my own.</div>
+        <div className="justify-center lg:justify-end text-center lg:text-right lg:w-1/2">
           Made with <FontAwesomeIcon icon={faCoffee} />, <FontAwesomeIcon icon={faCode} />, and lots
           of <FontAwesomeIcon icon={faHeart} />.
         </div>
       </div>
 
-      <div className="pb-5 flex flex-col lg:flex-row items-center">
-        <div className="lg:pl-4 lg:w-1/2">
-          Copyright &#169; 2019&ndash;{DateTime.now().year} Stefanie Molin, All rights reserved.
+      <div className="pb-5 flex flex-col lg:flex-row items-center lg:px-2">
+        <div className="lg:w-1/2">
+          Copyright &#169; 2019&ndash;{DateTime.now().year} Stefanie Molin. All rights reserved.
         </div>
-        <div className="justify-center lg:justify-end text-center lg:text-right lg:pl-4 lg:w-1/2">
+        <div className="justify-center lg:justify-end text-center lg:text-right lg:w-1/2">
           <FooterLinks />
         </div>
       </div>
--- app/components/subscribe-to-newsletter.tsx
+++ app/components/subscribe-to-newsletter.tsx
@@ -4,6 +4,8 @@ import {
   faEnvelope,
   faEnvelopeOpen,
   faEnvelopeOpenText,
+  faPerson,
+  faUser,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
@@ -60,7 +62,7 @@ const SubscribeToNewsletterForm = () => {
     <div className="flex flex-col my-2 grow">
       {status === 'SUCCESS' && (
         <>
-          <p>Welcome aboard{name ? `, ${name}` : ''}!</p>
+          <p>Welcome, {name ? `, ${name}` : ''}!</p>
           <p>Please check your inbox to confirm the subscription.</p>
         </>
       )}
@@ -73,74 +75,236 @@ const SubscribeToNewsletterForm = () => {
         </>
       )}
       {status === null && (
-        <form className="text-left" onSubmit={handleSubmit} action={FORM_URL}>
-          <div className="text-xl font-bold text-left">
+        <form className="text-center" onSubmit={handleSubmit} action={FORM_URL}>
+          <div className="text-xl font-bold">
             {/* <span className="fa-stack fa-layers fa-fw">
-              <FontAwesomeIcon icon={faCircle} size="2xl" fixedWidth /> */}
+                <FontAwesomeIcon icon={faCircle} size="2xl" fixedWidth /> */}
             {/* <FontAwesomeIcon
-              icon={faEnvelopeOpenText}
-              fixedWidth
-              // className="fa-inverse"
-            /> */}
+                icon={faEnvelopeOpenText}
+                fixedWidth
+                // className="fa-inverse"
+              /> */}
             {/* </span> */}
             Subscribe to my newsletter
           </div>
-          <div className="mt-5 flex flex-row justify-left">
-            {/* <label className="relative text-gray-400 focus-within:text-gray-600 block">
-              <input
-                aria-label="Your first name"
-                name="fields[first_name]"
-                placeholder="Your first name"
-                type="text"
-                onChange={handleNameChange}
-                value={name}
-                className={inputStyle}
-              />
-            </label> */}
-            <div className="grow">
-              <label className="relative text-gray-400 focus-within:text-gray-600 block">
-                {/* <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="pointer-events-none w-8 h-8 absolute top-1/2 transform -translate-y-1/2 left-3"
-                viewBox="0 0 20 20"
-                fill="currentColor">
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-              </svg> */}
-                <div className="inline-flex items-center w-full pr-2">
-                  <FontAwesomeIcon icon={faEnvelope} className="absolute ml-4 my-auto" size="xl" />
+          {/* <div>
+              <fieldset className="flex flex-row justify-around items-center">
+                <legend>Select which kinds of content you would like to hear about:</legend>
 
+                <div className="space-x-2">
+                  <input type="checkbox" id="articles" name="articles" checked />
+                  <label htmlFor="articles">Articles</label>
+                </div>
+
+                <div className="space-x-2">
+                  <input type="checkbox" id="blog" name="blog" />
+                  <label htmlFor="blog">Blog</label>
+                </div>
+                <div className="space-x-2">
+                  <input type="checkbox" id="books" name="books" />
+                  <label htmlFor="books">Books</label>
+                </div>
+                <div className="space-x-2">
+                  <input type="checkbox" id="workshops" name="workshops" />
+                  <label htmlFor="workshops">Workshops</label>
+                </div>
+              </fieldset>
+            </div> */}
+          <div className="flex flex-row items-center space-x-2">
+            <div className="mt-5 lg:mt-2 grow flex flex-col space-y-2 justify-left pr-2 lg:pr-0">
+              <label className="relative text-gray-400 focus-within:text-gray-600 block">
+                <div className="inline-flex items-center w-full">
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    className="absolute ml-4 my-auto"
+                    size="xl"
+                    fixedWidth
+                  />
                   <input
-                    required
-                    type="email"
-                    name="email_address"
-                    id="email"
-                    aria-label="Your email address"
-                    placeholder="email@example.com"
-                    onChange={handleEmailChange}
-                    value={email}
+                    aria-label="Your first name"
+                    name="fields[first_name]"
+                    id="name"
+                    placeholder="Your first name"
+                    type="text"
+                    onChange={handleNameChange}
+                    value={name}
                     className={inputStyle}
                   />
                 </div>
               </label>
-            </div>
+              <div className="grow">
+                <label className="relative text-gray-400 focus-within:text-gray-600 block">
+                  {/* <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="pointer-events-none w-8 h-8 absolute top-1/2 transform -translate-y-1/2 left-3"
+                  viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                </svg> */}
+                  <div className="inline-flex items-center w-full">
+                    <FontAwesomeIcon
+                      icon={faEnvelope}
+                      className="absolute ml-4 my-auto"
+                      size="xl"
+                      fixedWidth
+                    />
 
-            <button className="px-2 py-1 bg-slate-400 hover:bg-slate-500" type="submit">
+                    <input
+                      required
+                      type="email"
+                      name="email_address"
+                      id="email"
+                      aria-label="Your email address"
+                      placeholder="email@example.com"
+                      onChange={handleEmailChange}
+                      value={email}
+                      className={inputStyle}
+                    />
+                  </div>
+                </label>
+              </div>
+
+              <button className="px-2 py-1 bg-slate-400 hover:bg-slate-500" type="submit">
+                Subscribe
+              </button>
+            </div>
+            {/* <button className="px-2 py-1 rounded bg-slate-400 hover:bg-slate-500" type="submit">
               Subscribe
-            </button>
+            </button> */}
           </div>
-          {/* <div className="shrink my-2">
-            <button
-              className="px-2 py-1 rounded bg-slate-400 hover:bg-slate-500"
-              type="submit">
-              Subscribe
-            </button>
-          </div> */}
-          <small className="text-slate-500">I respect your privacy. Unsubscribe at any time.</small>
+          <div className="text-center">
+            <small className="text-slate-500">
+              I respect your privacy. Unsubscribe at any time.
+            </small>
+          </div>
         </form>
       )}
     </div>
   );
 };
+
+//   return (
+//     <div className="flex flex-col my-2 grow">
+//       {status === 'SUCCESS' && (
+//         <>
+//           <p>Welcome, {name ? `, ${name}` : ''}!</p>
+//           <p>Please check your inbox to confirm the subscription.</p>
+//         </>
+//       )}
+//       {status === 'ERROR' && (
+//         <>
+//           <p>Oops, something went wrong...</p>
+//           <p>
+//             Please, <button onClick={() => setStatus(null)}>try again.</button>
+//           </p>
+//         </>
+//       )}
+//       {status === null && (
+//         <form className="text-left" onSubmit={handleSubmit} action={FORM_URL}>
+//           <div className="text-xl font-bold">
+//             {/* <span className="fa-stack fa-layers fa-fw">
+//               <FontAwesomeIcon icon={faCircle} size="2xl" fixedWidth /> */}
+//             {/* <FontAwesomeIcon
+//               icon={faEnvelopeOpenText}
+//               fixedWidth
+//               // className="fa-inverse"
+//             /> */}
+//             {/* </span> */}
+//             Subscribe to my newsletter
+//           </div>
+//           <small className="text-slate-500">I respect your privacy. Unsubscribe at any time.</small>
+//           {/* <div>
+//             <fieldset className="flex flex-row justify-around items-center">
+//               <legend>Select which kinds of content you would like to hear about:</legend>
+
+//               <div className="space-x-2">
+//                 <input type="checkbox" id="articles" name="articles" checked />
+//                 <label htmlFor="articles">Articles</label>
+//               </div>
+
+//               <div className="space-x-2">
+//                 <input type="checkbox" id="blog" name="blog" />
+//                 <label htmlFor="blog">Blog</label>
+//               </div>
+//               <div className="space-x-2">
+//                 <input type="checkbox" id="books" name="books" />
+//                 <label htmlFor="books">Books</label>
+//               </div>
+//               <div className="space-x-2">
+//                 <input type="checkbox" id="workshops" name="workshops" />
+//                 <label htmlFor="workshops">Workshops</label>
+//               </div>
+//             </fieldset>
+//           </div> */}
+//           <div className="flex flex-row items-center space-x-2">
+//             <div className="mt-5 lg:mt-2 grow flex flex-row lg:flex-col space-x-2 lg:space-x-0 lg:space-y-2 justify-left pr-2 lg:pr-0">
+//               <label className="relative text-gray-400 focus-within:text-gray-600 block">
+//                 <div className="inline-flex items-center w-full">
+//                   <FontAwesomeIcon
+//                     icon={faUser}
+//                     className="absolute ml-4 my-auto"
+//                     size="xl"
+//                     fixedWidth
+//                   />
+//                   <input
+//                     aria-label="Your first name"
+//                     name="fields[first_name]"
+//                     id="name"
+//                     placeholder="Your first name"
+//                     type="text"
+//                     onChange={handleNameChange}
+//                     value={name}
+//                     className={inputStyle}
+//                   />
+//                 </div>
+//               </label>
+//               <div className="grow">
+//                 <label className="relative text-gray-400 focus-within:text-gray-600 block">
+//                   {/* <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 className="pointer-events-none w-8 h-8 absolute top-1/2 transform -translate-y-1/2 left-3"
+//                 viewBox="0 0 20 20"
+//                 fill="currentColor">
+//                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+//                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+//               </svg> */}
+//                   <div className="inline-flex items-center w-full">
+//                     <FontAwesomeIcon
+//                       icon={faEnvelope}
+//                       className="absolute ml-4 my-auto"
+//                       size="xl"
+//                       fixedWidth
+//                     />
+
+//                     <input
+//                       required
+//                       type="email"
+//                       name="email_address"
+//                       id="email"
+//                       aria-label="Your email address"
+//                       placeholder="email@example.com"
+//                       onChange={handleEmailChange}
+//                       value={email}
+//                       className={inputStyle}
+//                     />
+//                   </div>
+//                 </label>
+//               </div>
+
+//               {/* <button className="px-2 py-1 bg-slate-400 hover:bg-slate-500" type="submit">
+//               Subscribe
+//             </button> */}
+//             </div>
+//             <button className="px-2 py-1 rounded bg-slate-400 hover:bg-slate-500" type="submit">
+//               Subscribe
+//             </button>
+//           </div>
+//           {/* <small className="text-slate-500">I respect your privacy. Unsubscribe at any time.</small> */}
+//         </form>
+//       )}
+//     </div>
+//   );
+// };
 
 export default SubscribeToNewsletterForm;
