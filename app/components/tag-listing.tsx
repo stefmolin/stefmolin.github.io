94f31c886228ad84ac680c4a82e106750e1965df
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-01-30 19:05:25 -0500

    Pull tag list logic into separate component.

---
+++ app/components/tag-listing.tsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+import Link from 'next/link';
+
+interface TagCount {
+  text: string;
+  value: number;
+}
+
+export interface TagListingProps {
+  tagCounts: TagCount[];
+  className?: string;
+}
+
+const TagListing = ({ tagCounts, className }: TagListingProps) => {
+  return (
+    <div className={classNames(className)}>
+      {...tagCounts
+        .sort((tag1, tag2) => (tag1.value > tag2.value ? -1 : 1))
+        .map(({ text, value }) => (
+          <Link
+            key={text}
+            href={`/tags/${text}`}
+            className="font-bold text-slate-500 hover:text-slate-800 hover:underline"
+          >
+            {text} ({value})
+          </Link>
+        ))}
+    </div>
+  );
+};
+
+export default TagListing;
--- app/pages/tags/index.tsx
+++ app/pages/tags/index.tsx
@@ -1,38 +1,20 @@
-import Link from 'next/link';
 import Container from '../../components/container';
 import Header from '../../components/header';
 import Layout from '../../components/layout';
+import TagListing, { TagListingProps } from '../../components/tag-listing';
 import { getAllPosts } from '../../lib/api';
 
-type TagCount = {
-  text: string;
-  value: number;
-};
-
-type TagListingProps = {
-  tagCounts: TagCount[];
-};
-
-export default function TagListing({ tagCounts }: TagListingProps) {
+export default function Index({ tagCounts }: Pick<TagListingProps, 'tagCounts'>) {
   const preview = false;
   return (
     <Layout preview={preview}>
       <Container>
         <Header />
-        <h1 className="text-3xl">Post Tags</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-left pt-10 px-12 mb-10 md:mb-0">
-          {...tagCounts
-            .sort((tag1, tag2) => (tag1.value > tag2.value ? -1 : 1))
-            .map(({ text, value }) => (
-              <Link
-                key={text}
-                href={`/tags/${text}`}
-                className="font-bold text-slate-500 hover:text-slate-800 hover:underline"
-              >
-                {text} ({value})
-              </Link>
-            ))}
-        </div>
+        <h1 className="text-3xl">Search posts by tag</h1>
+        <TagListing
+          tagCounts={tagCounts}
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-left pt-10 px-12 mb-10 md:mb-0"
+        />
       </Container>
     </Layout>
   );
