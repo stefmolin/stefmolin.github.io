4d48ef2b488dd1d0fd0f9a9fcdc8de9774c3054c
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-01 23:32:21 -0500

    Update book interfaces

--- app/interfaces/book.ts
+++ app/interfaces/book.ts
@@ -8,15 +8,25 @@ export default interface Book {
   description: string;
   amazonLink: string;
   repoLink: string;
+  isbn: string;
+  tags: string[];
 }
 
 export interface TranslatedBook extends Pick<Book, 'publicationDate' | 'coverImage'> {
   language: string;
 }
 
+// TODO: this is probably general enough to be moved to another file (applies to workshops for example)
+export interface Review {
+  text: string;
+  author: string;
+}
+
 export interface RelatedContentLink {
   link: string;
-  contentClass: 'post' | 'book' | 'workshop';
+  contentClass: 'post' | 'book' | 'workshop'; // TODO: might not be necessary
+  image: string;
+  title: string;
 }
 
 export interface FAQ {
