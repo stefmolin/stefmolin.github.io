369d3f8cb84331822e59779e161125063d3c6ab8
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-01 23:27:38 -0500

    Allow custom formats with <DateFormatter />

--- app/components/date-formatter.tsx
+++ app/components/date-formatter.tsx
@@ -4,18 +4,21 @@ import { DateTime } from 'luxon';
 type Props = {
   dateString: string;
   relative?: boolean;
+  format?: string | typeof DateTime.DATETIME_FULL;
   long?: boolean;
   children?: ReactNode[];
 };
 
-const DateFormatter = ({ dateString, relative, long, children }: Props) => {
+const DateFormatter = ({ dateString, relative, format, long, children }: Props) => {
   const date = DateTime.fromISO(dateString);
   return (
     <time dateTime={dateString}>
       {...children ?? []}
       {date > DateTime.now().minus({ days: 2 }) && relative
         ? date.toRelative()
-        : date.toLocaleString(long ? DateTime.DATETIME_FULL : DateTime.DATE_FULL)}
+        : typeof format === 'string'
+          ? date.toFormat(format)
+          : date.toLocaleString(format ?? long ? DateTime.DATETIME_FULL : DateTime.DATE_FULL)}
     </time>
   );
 };
