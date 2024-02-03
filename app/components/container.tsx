3949096db0951d8e7f761faf2c541d30efb547cc
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-03 11:23:12 -0500

    Add padding to container

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 const Container = ({ children }: Props) => {
-  return <div className="container mx-auto">{children}</div>;
+  return <div className="container mx-auto px-10">{children}</div>;
 };
 
 export default Container;
