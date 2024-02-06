904dfa165d29ea7b3210734e537d292bd00b466a
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-05 20:12:20 -0500

    Update markdown styles import

--- app/components/posts/post-body.tsx
+++ app/components/posts/post-body.tsx
@@ -1,4 +1,4 @@
-import markdownStyles from '../markdown-styles.module.css';
+import markdownStyles from './markdown-styles.module.css';
 import { ReactNode } from 'react';
 import { Tweet } from 'react-tweet';
 
