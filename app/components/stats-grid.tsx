529ea065de722a070fa448d51f999b744ecc6ac4
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-11 19:25:09 -0500

    Adjust stats grid for smaller screen layout

--- app/components/stats-grid.tsx
+++ app/components/stats-grid.tsx
@@ -10,19 +10,23 @@ export interface StatsGridProps {
 export default function StatsGrid({ stats, className }: StatsGridProps) {
   return (
     <div className={classNames('flex justify-center', className)}>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12">
         {stats.map(({ title, value, link }) => {
           if (value == null || value === 0) return null;
           const stat = (
             <>
-              <h3 className="text-7xl text-center px-2">{value}</h3>
+              <h3 className="text-6xl md:text-7xl text-center px-2">{value}</h3>
               <h3 className="text-center px-2">{title}</h3>
             </>
           );
           return (
             <div
               key={title}
-              className="flex flex-col shadow-sm hover:shadow-lg transition-shadow duration-200 w-48 h-48 justify-center items-center"
+              className={classNames(
+                'flex flex-col justify-center items-center',
+                'shadow-sm hover:shadow-lg transition-shadow duration-200',
+                'w-32 h-32 sm:w-48 sm:h-48',
+              )}
             >
               {link != null && usePathname() !== link ? (
                 <Link
