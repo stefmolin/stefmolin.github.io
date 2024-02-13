418b77454002108313542822c824f700cdca407a
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-12 20:03:22 -0500

    Add open prop to CollapsibleSection

--- app/components/collapsible-section.tsx
+++ app/components/collapsible-section.tsx
@@ -6,23 +6,26 @@ const CollapsibleSection = ({
   prompt,
   contents,
   children,
+  open = false,
 }: {
   prompt: string;
+  open?: boolean;
   contents?: string;
   children?: React.ReactNode;
 }) => (
   <div className="flex flex-col items-center space-y-1">
     <Collapsible
+      open={open}
       trigger={
         <>
           <FontAwesomeIcon icon={faChevronRight} className="pr-1" fixedWidth />
-          <span className="text-lg text-bold cursor-pointer">{prompt}</span>
+          <span className="text-xl cursor-pointer">{prompt}</span>
         </>
       }
       triggerWhenOpen={
         <>
           <FontAwesomeIcon icon={faChevronDown} className="pr-1" fixedWidth />
-          <span className="text-lg text-bold cursor-pointer">{prompt}</span>
+          <span className="text-xl cursor-pointer">{prompt}</span>
         </>
       }
     >
