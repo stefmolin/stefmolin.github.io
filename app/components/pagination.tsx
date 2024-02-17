d357e839b6fa1cf9ece8fa746f563c09004b92e1
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-16 19:02:10 -0500

    Restyle pagination

--- app/components/pagination.tsx
+++ app/components/pagination.tsx
@@ -1,5 +1,7 @@
-import classNames from 'classnames';
 import { Dispatch, MutableRefObject, SetStateAction } from 'react';
+import { faCaretLeft, faCaretRight, faEllipsis } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import ReactPaginate from 'react-paginate';
 
 interface PaginationProps {
@@ -19,20 +21,35 @@ export default function Pagination({
     setOffset((selected * itemsPerPage) % totalItems);
     if (scrollRef.current != null) scrollRef.current.scrollIntoView();
   };
-  const numberClassName = 'md:shadow px-3 md:px-5 py-2';
+  const numberClassName = 'px-3 md:px-5 py-2';
   return (
     <ReactPaginate
-      breakLabel="..."
-      previousLabel="<"
-      nextLabel=">"
+      breakLabel={
+        <>
+          <FontAwesomeIcon icon={faEllipsis} fixedWidth />
+        </>
+      }
+      previousLabel={
+        <>
+          <FontAwesomeIcon icon={faCaretLeft} fixedWidth />
+        </>
+      }
+      nextLabel={
+        <>
+          <FontAwesomeIcon icon={faCaretRight} fixedWidth />
+        </>
+      }
       onPageChange={onPageChange}
       pageRangeDisplayed={2}
       marginPagesDisplayed={2}
       pageCount={Math.ceil(totalItems / itemsPerPage)}
       renderOnZeroPageCount={null}
-      className="flex flex-row items-center justify-center space-x-1 text-base md:text-xl font-bold text-slate-800 py-5 md:px-5"
+      className={classNames(
+        'flex flex-row items-center justify-center space-x-1',
+        'text-base md:text-xl font-bold text-slate-800 py-5 md:px-5',
+      )}
       pageLinkClassName={classNames(numberClassName, 'hover:underline')}
-      activeLinkClassName="text-blue-700"
+      activeLinkClassName="text-blue-700 shadow-inner bg-slate-100 rounded-full"
       breakLinkClassName={numberClassName}
       nextLinkClassName={numberClassName}
       previousLinkClassName={numberClassName}
