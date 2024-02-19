3b40424d358fac351b6138b70d2f9378d61d81f9
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-19 14:24:56 -0500

    Include header in layout; make it possible to customize

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,17 +1,22 @@
+import classNames from 'classnames';
 import Alert from './alert';
 import Footer from './footer';
+import Header from './header';
 import Meta from './meta';
 
 type Props = {
   preview?: boolean;
   children: React.ReactNode;
+  className?: string;
+  styleProps?: Record<string, string | number>;
 };
 
-const Layout = ({ preview, children }: Props) => {
+const Layout = ({ preview, children, className, styleProps }: Props) => {
   return (
     <>
       <Meta />
-      <div className="min-h-screen">
+      <div className={classNames('min-h-screen', className)} style={styleProps}>
+        <Header />
         {/* <Alert preview={preview} /> TODO: this probably can be removed */}
         <main>{children}</main>
       </div>
