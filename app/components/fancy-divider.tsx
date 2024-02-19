07e1474720c59a1b377db8df3cfd8de174a2b8fd
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-18 21:38:49 -0500

    Add way to customize rules in <FancyDivider />

--- app/components/fancy-divider.tsx
+++ app/components/fancy-divider.tsx
@@ -3,14 +3,16 @@ import classNames from 'classnames';
 interface FancyDividerProps {
   children: React.ReactNode | React.ReactNode[];
   className?: string;
+  hrClassName?: string;
 }
 
-export default function FancyDivider({ children, className }: FancyDividerProps) {
+export default function FancyDivider({ children, className, hrClassName }: FancyDividerProps) {
+  const rule = <hr className={classNames('w-full', hrClassName)} />;
   return (
     <div className={classNames('flex flex-row items-center justify-center w-full', className)}>
-      <hr className="w-full" />
+      {rule}
       {children}
-      <hr className="w-full" />
+      {rule}
     </div>
   );
 }
