16ecd5ab1d9a9471cc6500b0f4460beb65bffb6e
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-22 21:49:38 -0500

    Add mobile-friendly header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,44 +1,99 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames';
+import Hamburger from 'hamburger-react';
 import Link from 'next/link';
-import { NEWSLETTER_URL } from '../lib/constants';
-// import Avatar from "./avatar";
+import { useState } from 'react';
+import { NEWSLETTER_URL } from '../data/constants';
 
-const Header = () => {
+function NavigationLinks({
+  className,
+  separatorClassName,
+  transition,
+}: {
+  className: string | Record<string, boolean>;
+  separatorClassName: string | Record<string, boolean>;
+  transition: string;
+}) {
+  const separator = <span className={classNames(separatorClassName)}>|</span>;
+  const linkClassName = 'hover:underline';
   return (
-    <>
-      {/* <div className="bg-neutral-50 border-t border-neutral-200">*/}
-      <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 text-right">
-        {/* <Link href="/blog" className="hover:underline">
-        <FontAwesomeIcon icon={faArrowLeft} /> Back to Stefanie Molin's blog.
-      </Link> */}
-        <Link href="/" className="hover:underline">
-          Home
-        </Link>
-        <Link href="/articles" className="hover:underline">
-          Articles
-        </Link>
-        {' | '}
-        <Link href="/blog" className="hover:underline">
-          Blog
-        </Link>
-        {' | '}
-        <Link href={NEWSLETTER_URL} className="hover:underline">
-          Newsletter
-        </Link>
-        {' | '}
-        <Link href="/talks" className="hover:underline">
-          Talks
-        </Link>
-        {' | '}
-        <Link href="/workshops" className="hover:underline">
-          Workshops
-        </Link>
-        {/* <Avatar name="Stefanie Molin" picture="/assets/avatar.jpeg" /> */}
-      </h2>
-      {/* </div> <- Put this back and update the post slug file to mirror the footer setup (with more tweaks) */}
-    </>
+    <div className={classNames(className)}>
+      <Link href="/articles" className={classNames(linkClassName, 'delay-100', transition)}>
+        Articles
+      </Link>
+      {separator}
+      <Link href="/blog" className={classNames(linkClassName, 'delay-150', transition)}>
+        Blog
+      </Link>
+      {separator}
+      <Link href="/books" className={classNames(linkClassName, 'delay-200', transition)}>
+        Books
+      </Link>
+      {separator}
+      <Link href="/events" className={classNames(linkClassName, 'delay-300', transition)}>
+        Events
+      </Link>
+      {separator}
+      <Link href={NEWSLETTER_URL} className={classNames(linkClassName, 'delay-500', transition)}>
+        Newsletter
+      </Link>
+      {separator}
+      <Link href="/workshops" className={classNames(linkClassName, 'delay-700', transition)}>
+        Workshops
+      </Link>
+    </div>
   );
-};
+}
 
-export default Header;
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={classNames(
+        'relative transition-height duration-500 ease-in-out',
+        'bg-neutral-50 border-b border-neutral-200',
+        'mb-20 sticky top-0 z-50 w-full',
+        {
+          'h-screen': isOpen,
+          'h-16': !isOpen,
+        },
+      )}
+    >
+      <div
+        className={classNames('py-2 text-2xl mx-5 flex flex-row justify-between h-16', {
+          'items-center': !isOpen,
+        })}
+      >
+        <div
+          className={classNames('flex flex-row justify-between w-full', {
+            'flex-row items-center': !isOpen,
+            'flex-col items-baseline': isOpen,
+          })}
+        >
+          <h2
+            className={classNames('transition-[font-size] duration-1000 ease-in-out', {
+              'text-5xl py-2': isOpen,
+            })}
+          >
+            <Link href="/" className="hover:underline font-bold">
+              Stefanie Molin
+            </Link>
+          </h2>
+          <NavigationLinks
+            className={classNames('flex opacity-0 w-0 h-0', {
+              'flex-row space-x-2 text-lg invisible md:visible md:h-auto md:w-auto md:opacity-100':
+                !isOpen,
+              'flex-col space-y-2 text-3xl pl-2 opacity-100 transition-opacity transition-spacing transform translate-y-6 duration-1000 ease-in':
+                isOpen,
+            })}
+            separatorClassName={{ hidden: isOpen }}
+            transition={isOpen ? '' : 'transition-none'}
+          />
+        </div>
+        <div className="md:hidden">
+          <Hamburger toggled={isOpen} toggle={setIsOpen} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -34,6 +34,10 @@ module.exports = {
         sm: '0 5px 10px rgba(0, 0, 0, 0.12)',
         md: '0 8px 30px rgba(0, 0, 0, 0.12)',
       },
+      transitionProperty: {
+        height: 'height',
+        spacing: 'margin, padding',
+      },
     },
   },
   plugins: [],
