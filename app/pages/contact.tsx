f1dfe21ee828170015bab66175749f3adc27866b
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-24 17:59:54 -0500

    Add contact page

---
+++ app/pages/contact.tsx
@@ -0,0 +1,46 @@
+import Container from '../components/sections/container';
+import Layout from '../components/page-layout/layout';
+import ExternalLink from '../components/links/external-link';
+import { HEADSHOT, LINKEDIN_PROFILE } from '../data/constants';
+import FancyDivider from '../components/dividers/fancy-divider';
+import FollowButtons from '../components/follow';
+
+export default function Index() {
+  return (
+    <Layout
+      className="bg-cover bg-fixed bg-center"
+      styleProps={{
+        backgroundImage: 'url(/assets/events/conferences/conference-badges.jpg)',
+      }}
+    >
+      <Container>
+        <div className="mt-4 max-w-3xl mx-auto p-10 bg-white rounded-lg shadow-lg">
+          <div className="flex flex-col items-center mb-2 space-y-4">
+            <h1 className="text-4xl">Contact Me</h1>
+            <img src={HEADSHOT} className="h-48 w-48 rounded-full" />
+          </div>
+
+          <div className="text-lg md:text-xl py-2">
+            <p>
+              Whether you would like to{' '}
+              <span className="font-bold">invite me to speak at your event</span>, have me as a{' '}
+              <span className="font-bold">guest on your podcast</span>, or simply{' '}
+              <span className="font-bold">share your thoughts on my content</span>, I would love to
+              hear from you. The best way to reach me is via{' '}
+              <ExternalLink
+                href={LINKEDIN_PROFILE}
+                className="py-px font-bold underline decoration-yellow-400 hover:text-slate-700"
+              >
+                LinkedIn
+              </ExternalLink>
+              .
+            </p>
+          </div>
+          <FancyDivider className="py-5">
+            <FollowButtons size="3x" className="px-5" withoutNewsletter />
+          </FancyDivider>
+        </div>
+      </Container>
+    </Layout>
+  );
+}
