725f43e514e9463282f4f7117eccce4dc3e8204b
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-25 23:02:56 -0500

    Adjust announcement component to h-full and less pading on smaller screens

--- app/components/cards/announcement.tsx
+++ app/components/cards/announcement.tsx
@@ -20,7 +20,7 @@ export default function Announcement({ children, className, hrClassName }: Annou
         className,
       )}
     >
-      <div className="space-y-4 px-10 w-full">
+      <div className="space-y-4 px-6 sm:px-10 w-full h-full">
         <FancyDivider hrClassName={hrClassName}>
           <FontAwesomeIcon icon={faThumbTack} className="px-5" fixedWidth />
         </FancyDivider>
