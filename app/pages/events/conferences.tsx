6cf1e7b906f180a5de4750801f921cc823e46e3f
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-26 18:13:25 -0500

    Add intro for conferences page

--- app/pages/events/conferences.tsx
+++ app/pages/events/conferences.tsx
@@ -1,18 +1,24 @@
-import { NextSeo } from 'next-seo';
+import classNames from 'classnames';
 import _ from 'lodash';
-import Container from '../../components/sections/container';
-import Layout from '../../components/page-layout/layout';
-import { usePageURL } from '../../lib/hooks/page-url';
-import SectionSeparator from '../../components/dividers/section-separator';
+import { DateTime } from 'luxon';
+import Link from 'next/link';
+import { NextSeo } from 'next-seo';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLightbulb } from '@fortawesome/free-regular-svg-icons';
+import FancyDivider from '../../components/dividers/fancy-divider';
 import EventMap from '../../components/events/event-map';
-import CONTENT_LINKS from '../../data/content-links';
-import { getImageLink } from '../../lib/images';
-import { LIVE_PRESENTATIONS } from '../../data/events';
-import RelatedContentSection from '../../components/related-content/related-content';
-import { CONFERENCE_IMAGES } from '../../data/photo-gallery';
-import PhotoGallery from '../../components/photo-gallery';
 import EventStatsSection from '../../components/events/event-stats-section';
+import Layout from '../../components/page-layout/layout';
+import PhotoGallery from '../../components/photo-gallery';
+import RelatedContentSection from '../../components/related-content/related-content';
+import Container from '../../components/sections/container';
+import EvenlySpacedSections from '../../components/sections/evenly-spaced-sections';
 import { MAP_PIN } from '../../data/constants';
+import CONTENT_LINKS from '../../data/content-links';
+import { LIVE_PRESENTATIONS } from '../../data/events';
+import { CONFERENCE_IMAGES } from '../../data/photo-gallery';
+import { usePageURL } from '../../lib/hooks/page-url';
+import { getImageLink } from '../../lib/images';
 
 const relatedContent = [
   CONTENT_LINKS.WORKSHOPS,
@@ -20,8 +26,6 @@ const relatedContent = [
   CONTENT_LINKS.BOOK_SIGNINGS,
   CONTENT_LINKS.TALKS,
 ];
-
-// TODO: add CTA to invite me to speak (could link to /contact)
 
 export default function Conferences() {
   const seoImage = CONTENT_LINKS.CONFERENCES.image;
@@ -57,18 +61,74 @@ export default function Conferences() {
           }}
         />
         <div className="mt-4 mb-20 max-w-5xl mx-auto">
-          <h1 className="text-5xl mb-2">{pageTitle}</h1>
-          <PhotoGallery photos={CONFERENCE_IMAGES} title={null} shufflePhotos />
-          <SectionSeparator className="my-10" />
-          <EventStatsSection sessions={presentations} includeYearsActive />
-          <SectionSeparator className="my-10" />
-          <EventMap
-            introText={`To date, I have presented ${presentations.length} times at conferences around
+          <h1 className="text-5xl md:text-7xl mb-2 text-center">{pageTitle}</h1>
+
+          <EvenlySpacedSections className="my-10">
+            <div className="flex flex-col items-center">
+              <FancyDivider className="w-1/2 mx-auto pb-5">
+                <FontAwesomeIcon icon={faLightbulb} className="px-5" fixedWidth />
+              </FancyDivider>
+              <p className="md:text-justify text-lg lg:text-xl px-2 md:px-10">
+                <b>Knowledge sharing is a passion of mine</b>. To date, I have delivered{' '}
+                {presentationsGivenAlready} presentations at conferences around the world. On this
+                page you will find {anchorLink('#photos', 'photos')} from past presentations, a{' '}
+                {anchorLink('#event-map', 'map')} showing locations and dates of past events, some{' '}
+                {anchorLink('#stats', 'statistics')} about these events, along with a note about{' '}
+                {anchorLink('#origin-story', 'how I got started')} with these events. If you would
+                like to <b>invite me to speak</b> at your event, please{' '}
+                {
+                  <Link href="/contact" className={classNames(linkClassName, 'font-bold')}>
+                    get in touch
+                  </Link>
+                }
+                .
+              </p>
+            </div>
+            <PhotoGallery photos={CONFERENCE_IMAGES} shufflePhotos />
+            <EventStatsSection sessions={presentations} includeYearsActive />
+            <EventMap
+              introText={`To date, I have presented ${presentationsGivenAlready} times at conferences around
               the world. Click a ${MAP_PIN} on the map for more information.`}
-            liveEvents={presentations}
-          />
-          <SectionSeparator className="my-10" />
-          <RelatedContentSection relatedContent={relatedContent} />
+              liveEvents={presentations}
+            />
+            <div
+              id="origin-story"
+              className="text-lg border-2 border-slate-100 rounded-lg px-4 bg-slate-50 shadow-lg xl:w-5/6 mx-auto"
+            >
+              <p className="pt-10 px-10 text-2xl">My origin story</p>
+              <div className="mt-5 px-10 py-5 text-justify space-y-4">
+                <p>
+                  I used to be TERRIFIED of public speaking. After writing my{' '}
+                  <Link href={CONTENT_LINKS.PANDAS_BOOK_1.link} className={linkClassName}>
+                    first book
+                  </Link>
+                  , I was approached with an opportunity to speak at a virtual conference in 2021. I
+                  did want to conquer this fear, and the idea of it being virtual was a huge plus at
+                  the time. That first event proved to be a pivotal moment that helped me gain
+                  enough confidence to attempt an in-person event. It took several more
+                  presentations before I truly got comfortable, though.
+                </p>
+
+                <img
+                  src="/assets/events/conferences/conference-badges.jpg"
+                  className="w-48 h-48 rounded-full mx-auto z-10 object-cover"
+                  alt="Signing a book at my first book signing event."
+                />
+                <p>
+                  Now, I absolutely love it! I get to meet and help people from all over the world
+                  by sharing my knowledge. I save each and every one of my conference speaker badges
+                  &ndash; they are my trophies, and conquering this fear is one of the things I am
+                  most proud of.
+                </p>
+                <p className="pb-5 text-right">
+                  &ndash; <em>Stefanie</em>
+                </p>
+              </div>
+
+              <br />
+            </div>
+            <RelatedContentSection carouselClassName="pt-2" relatedContent={relatedContent} />
+          </EvenlySpacedSections>
         </div>
       </Container>
     </Layout>
