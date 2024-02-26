fc2be74b1b78e4deedd744112b93df6295f3dca1
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-25 23:09:17 -0500

    Reformatting; add CTAs.

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -10,6 +10,7 @@ import classNames from 'classnames';
 import { DateTime } from 'luxon';
 import Link from 'next/link';
 import seedrandom from 'seedrandom';
+import Announcement from '../components/cards/announcement';
 import FollowButtons from '../components/follow';
 import ResourceLink from '../components/links/resource-link';
 import Layout from '../components/page-layout/layout';
@@ -18,6 +19,7 @@ import RelatedContentSection from '../components/related-content/related-content
 import Container from '../components/sections/container';
 import EvenlySpacedSections from '../components/sections/evenly-spaced-sections';
 import PageSection from '../components/sections/page-section';
+import SubscribeToNewsletterForm from '../components/subscribe-to-newsletter';
 import { FLAGS, HEADSHOT } from '../data/constants';
 import CONTENT_LINKS from '../data/content-links';
 import { LIVE_PRESENTATIONS } from '../data/events';
@@ -41,11 +43,13 @@ const FeaturedPost = ({
   icon,
   post,
   title,
+  titleClassName,
 }: {
   feedType: 'article' | 'blog';
   icon: IconDefinition;
   post: PostType;
   title: string;
+  titleClassName: string;
 }) => (
   <PageSection
     title={
@@ -54,7 +58,7 @@ const FeaturedPost = ({
         {title}*
       </>
     }
-    titleClassName="text-5xl text-center"
+    titleClassName={classNames('text-center', titleClassName)}
   >
     <div className="grid grid-cols-1">
       <PostPreview {...post} coverImage={post.ogImage.url} />
@@ -86,25 +90,61 @@ export default function Home({
   const articleOfTheDay =
     articles[Math.floor(seedrandom(DateTime.now().startOf('day'))() * articles.length)];
   const nextSessions = getNextSessions(LIVE_PRESENTATIONS);
+  const subsectionHeaderClassName = 'text-2xl sm:text-3xl md:text-5xl';
+  const inviteMeToSpeakCTA = (
+    <Announcement>
+      <div className="flex flex-col items-center justify-between space-y-4">
+        <h3 className="text-lg text-center sm:text-xl font-bold mt-2">
+          Invite me to speak at your event
+        </h3>
+        <FontAwesomeIcon icon={faMicrophoneLines} fixedWidth size="2x" />
+        <p>
+          If you would like to invite me to speak at your event or be a guest on your podcast,
+          please{' '}
+          <Link href="/contact" className="font-bold underline text-slate-600 hover:text-black">
+            reach out
+          </Link>
+          .
+        </p>
+      </div>
+    </Announcement>
+  );
+  const newsletterCTA = (
+    <Announcement>
+      <div className="w-full flex flex-col items-center">
+        <SubscribeToNewsletterForm />
+      </div>
+    </Announcement>
+  );
   return (
     <Layout>
       <Container>
         <div className="-mt-8 mb-20 max-w-5xl mx-auto">
           <EvenlySpacedSections className="my-10">
             <div className="text-center">
-              <h1 className="text-5xl md:text-7xl">Stefanie Molin</h1>
+              <h1 className="text-3xl sm:text-5xl md:text-7xl mb-4 md:mb-8">
+                <Link className="hover:underline" href="/about">
+                  Stefanie Molin
+                </Link>
+              </h1>
               <div className="flex flex-col items-center justify-center">
-                <img
-                  src={HEADSHOT}
-                  className="w-48 h-48 rounded-full object-cover mb-2"
-                  alt="Picture of Stefanie Molin."
-                />
+                <Link href="/about">
+                  <img
+                    src={HEADSHOT}
+                    className="w-48 h-48 rounded-full object-cover"
+                    alt="Picture of Stefanie Molin."
+                  />
+                </Link>
               </div>
-              <h2 className="text-2xl md:text-4xl">
+              <h2 className="text-lg sm:text-2xl md:text-4xl mt-4">
                 Software Engineer | Author | International Speaker
               </h2>
-              <FollowButtons size="3x" className="px-5" withDivider dividerClassName="py-5" />
-              <p className="text-2xl">
+              <FollowButtons
+                className="text-xl md:text-3xl px-5"
+                withDivider
+                dividerClassName="py-5"
+              />
+              <p className=" sm:text-xl md:text-2xl">
                 I help people of all levels improve their data science and computer science skills.
               </p>
               <RelatedContentSection
@@ -121,9 +161,9 @@ export default function Home({
                     Upcoming Live Sessions*
                   </>
                 }
-                titleClassName="text-5xl text-center"
+                titleClassName={classNames('text-center', subsectionHeaderClassName)}
               >
-                <div className="flex w-full place-content-around">
+                <div className="flex w-full place-content-around mt-2">
                   <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
                     {nextSessions.map((session) => {
                       const eventDate = DateTime.fromISO(session.date);
@@ -131,7 +171,13 @@ export default function Home({
                       return (
                         <div
                           key={`${session.date}-${session.presentation.title}`}
-                          className="bg-white h-60 sm:h-48 w-auto lg:w-96 shadow-sm rounded p-4 m-4 flex flex-col items-center justify-between"
+                          className={classNames(
+                            'h-auto lg:h-48 w-auto lg:w-96',
+                            'flex flex-col items-center justify-between space-y-4',
+                            'bg-white border-2 border-slate-700',
+                            'shadow-sm rounded-lg',
+                            'p-4 my-4 sm:m-4',
+                          )}
                         >
                           <ResourceLink
                             className="text-slate-600 hover:underline"
@@ -146,15 +192,29 @@ export default function Home({
                               {session.presentation.title}
                             </h2>
                           </ResourceLink>
-                          <h3 className="text-lg sm:text-xl">
-                            <FontAwesomeIcon icon={faCalendarCheck} fixedWidth />{' '}
-                            {DateTime.fromISO(session.date).toLocaleString()} ({relativeEventDate})
+                          <h3 className="text-lg sm:text-xl text-center">
+                            <div className="flex flex-col sm:flex-row items-center justify-center space-x-1">
+                              <div>
+                                <FontAwesomeIcon
+                                  className="pr-1"
+                                  icon={faCalendarCheck}
+                                  fixedWidth
+                                />
+                                {DateTime.fromISO(session.date).toLocaleString()}
+                              </div>
+                              <div>({relativeEventDate})</div>
+                            </div>
                           </h3>
-                          <div className="flex flex-col sm:flex-row items-center justify-between w-full">
-                            <p>
-                              {FLAGS[session.event.location.country]} {session.event.name} (
-                              {session.event.location.city})
-                            </p>
+                          <div className="flex flex-col-reverse sm:flex-row items-center justify-between w-full">
+                            <div className="flex flex-col sm:flex-row items-center justify-center space-x-1 text-center">
+                              <div>
+                                <span className="pr-1">
+                                  {FLAGS[session.event.location.country]}
+                                </span>{' '}
+                                {session.event.name}
+                              </div>
+                              <div className="hidden sm:flex">({session.event.location.city})</div>
+                            </div>
                             <p>
                               <FontAwesomeIcon
                                 icon={
@@ -183,17 +243,23 @@ export default function Home({
                 </div>
               </PageSection>
             )}
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-12 sm:mx-4">
+              {inviteMeToSpeakCTA}
+              {newsletterCTA}
+            </div>
             <FeaturedPost
               feedType="article"
               icon={faNewspaper}
               post={articleOfTheDay}
               title="Article of the Day"
+              titleClassName={subsectionHeaderClassName}
             />
             <FeaturedPost
               feedType="blog"
               icon={faRss}
               post={latestBlogPost}
               title="Latest Blog Post"
+              titleClassName={subsectionHeaderClassName}
             />
           </EvenlySpacedSections>
         </div>
