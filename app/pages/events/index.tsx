6bb70e959a06bf84ea0609d29288f70a5c289172
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-02-26 18:57:19 -0500

    Add intro to events page

--- app/pages/events/index.tsx
+++ app/pages/events/index.tsx
@@ -1,17 +1,22 @@
+import { faLightbulb } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import Link from 'next/link';
 import { NextSeo } from 'next-seo';
-import Container from '../../components/sections/container';
-import Layout from '../../components/page-layout/layout';
-import { usePageURL } from '../../lib/hooks/page-url';
-import SectionSeparator from '../../components/dividers/section-separator';
+import FancyDivider from '../../components/dividers/fancy-divider';
 import EventMap from '../../components/events/event-map';
-import CONTENT_LINKS from '../../data/content-links';
-import { getImageLink } from '../../lib/images';
-import { LIVE_PRESENTATIONS } from '../../data/events';
-import RelatedContentSection from '../../components/related-content/related-content';
-import { EVENT_IMAGES } from '../../data/photo-gallery';
-import PhotoGallery from '../../components/photo-gallery';
 import EventStatsSection from '../../components/events/event-stats-section';
+import Layout from '../../components/page-layout/layout';
+import PhotoGallery from '../../components/photo-gallery';
+import RelatedContentSection from '../../components/related-content/related-content';
+import Container from '../../components/sections/container';
+import EvenlySpacedSections from '../../components/sections/evenly-spaced-sections';
 import { MAP_PIN } from '../../data/constants';
+import CONTENT_LINKS from '../../data/content-links';
+import { LIVE_PRESENTATIONS } from '../../data/events';
+import { EVENT_IMAGES } from '../../data/photo-gallery';
+import { usePageURL } from '../../lib/hooks/page-url';
+import { getImageLink } from '../../lib/images';
 
 const relatedContent = [
   CONTENT_LINKS.CONFERENCES,
@@ -22,12 +27,18 @@ const relatedContent = [
   CONTENT_LINKS.BLOG,
 ];
 
-// TODO: add CTA to invite me to speak (could link to /contact)
 // TODO: incorporate the non-conference, non-signing events?
 
 export default function Index() {
   const seoImage = CONTENT_LINKS.EVENTS.image;
   const pageTitle = 'Events';
+  const subsectionTitleClassName = 'text-center md:text-left text-3xl mb-5';
+  const linkClassName = 'text-slate-700 hover:text-slate-500 underline';
+  const anchorLink = (fragment: string, text: string | React.ReactNode) => (
+    <Link href={fragment} className={linkClassName}>
+      {text}
+    </Link>
+  );
   return (
     <Layout>
       <Container>
@@ -47,18 +58,46 @@ export default function Index() {
           }}
         />
         <div className="mt-4 mb-20 max-w-5xl mx-auto">
-          <h1 className="text-5xl mb-2">{pageTitle}</h1>
-          <SectionSeparator className="my-10" />
-          <PhotoGallery photos={EVENT_IMAGES} title="Photo gallery 📷" shufflePhotos />
-          <SectionSeparator className="my-10" />
-          <EventStatsSection sessions={LIVE_PRESENTATIONS} />
-          <SectionSeparator className="my-10" />
-          <EventMap
-            introText={`Click a ${MAP_PIN} on the map to see the events I have participated in around the world.`}
-            liveEvents={LIVE_PRESENTATIONS}
-          />
-          <SectionSeparator className="my-10" />
-          <RelatedContentSection relatedContent={relatedContent} />
+          <h1 className="text-6xl md:text-7xl mb-2 text-center">{pageTitle}</h1>
+          <EvenlySpacedSections className="my-10">
+            <div className="flex flex-col items-center">
+              <FancyDivider className="w-1/2 mx-auto pb-5">
+                <FontAwesomeIcon icon={faLightbulb} className="px-5" fixedWidth />
+              </FancyDivider>
+              <p className="md:text-justify text-lg lg:text-xl px-2 md:px-10">
+                <b>Knowledge sharing is a passion of mine</b>. On this page you will find{' '}
+                {anchorLink('#photos', 'photos')} from past <b>conference presentations</b> and{' '}
+                <b>book signings</b>, a {anchorLink('#event-map', 'map')} showing event locations
+                and dates, along with some {anchorLink('#stats', 'statistics')} about these events.
+                If you would like to <b>invite me to speak</b> at your event, please{' '}
+                {
+                  <Link href="/contact" className={classNames(linkClassName, 'font-bold')}>
+                    get in touch
+                  </Link>
+                }
+                .
+              </p>
+            </div>
+            <PhotoGallery
+              photos={EVENT_IMAGES}
+              shufflePhotos
+              titleClassName={subsectionTitleClassName}
+            />
+            <EventStatsSection
+              sessions={LIVE_PRESENTATIONS}
+              titleClassName={subsectionTitleClassName}
+            />
+            <EventMap
+              introText={`Click a ${MAP_PIN} on the map to see the events I have participated in
+              around the world.`}
+              liveEvents={LIVE_PRESENTATIONS}
+              titleClassName={subsectionTitleClassName}
+            />
+            <RelatedContentSection
+              relatedContent={relatedContent}
+              titleClassName={subsectionTitleClassName}
+            />
+          </EvenlySpacedSections>
         </div>
       </Container>
     </Layout>
