852fe33b69cd3ba269d5d2c7edea4b53742de471
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-03-05 19:02:09 -0500

    Add initial favicon

--- app/components/page-layout/meta.tsx
+++ app/components/page-layout/meta.tsx
@@ -38,15 +38,27 @@ const Meta = () => {
   return (
     <Head>
       {getFeedLinks()}
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/favicon/android-chrome-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="512x512"
+        href="/favicon/android-chrome-512x512.png"
+      />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      // TODO: these can move to the SEO config once finalized
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      {/* <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" /> */}
+      {/* <meta name="msapplication-TileColor" content="#000000" /> */}
+      {/* <meta name="msapplication-config" content="/favicon/browserconfig.xml" /> */}
+      {/* <meta name="theme-color" content="#ffffff" /> */}
     </Head>
   );
 };
