3a568a6dd03451c91cdde655ebb69e86cfac484e
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-07-03 19:16:51 -0400

    Add MentionStats component

---
+++ app/components/mention-stats.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import numeral from 'numeral';
+import ExternalLink from './links/external-link';
+import { usePageURL } from '../lib/hooks/page-url';
+
+interface Mentions {
+  count: number;
+  type: Record<string, number>;
+}
+
+export default function MentionStats({ className }: { className?: string }) {
+  const [mentions, setMentions] = useState<Mentions | null>(null);
+  const pageURL = usePageURL();
+
+  useEffect(() => {
+    if (pageURL)
+      fetch(`https://webmention.io/api/count?target=${pageURL}`)
+        .then((response) => response.json())
+        .then((data) => setMentions(data))
+        .catch(() => setMentions(null));
+  }, [pageURL]);
+
+  return mentions == null || mentions.count === 0 ? null : (
+    <div
+      className={
+        className ??
+        'flex flex-col sm:flex-row items-center text-center sm:justify-center sm:space-x-2'
+      }
+    >
+      <ExternalLink
+        href={`https://webmention.io/api/mentions.html?target=${pageURL}`}
+        className="hover:underline"
+      >
+        <span>
+          <FontAwesomeIcon icon={faHeart} className="pr-1" fixedWidth />
+          {numeral(mentions.count).format('0,0')} mentions
+        </span>
+      </ExternalLink>
+    </div>
+  );
+}
