0dafe1eaed42cca260669e52ece50568fdd2c771
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-07-09 01:00:27 +0200

    Remove unnecessary fragment

--- app/pages/feedback.tsx
+++ app/pages/feedback.tsx
@@ -2,12 +2,10 @@ import EmbeddedPage from '../components/page-layout/embed';
 
 export default function Feedback() {
   return (
-    <>
-      <EmbeddedPage
-        src="https://tally.so/r/w8edyP"
-        pageTitle="Live Session Feedback Form"
-        description="Feedback form for all of Stefanie Molin's live sessions."
-      />
-    </>
+    <EmbeddedPage
+      src="https://tally.so/r/w8edyP"
+      pageTitle="Live Session Feedback Form"
+      description="Feedback form for all of Stefanie Molin's live sessions."
+    />
   );
 }
