459d08cd9f372889f4edb3b3ace3226350e0f5c7
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-08-29 10:33:18 +0200

    Update country count

--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -138,9 +138,9 @@ export default function AboutMe() {
                   <p>
                     I am an <b>avid traveler</b> based in the <b>New York City</b> metropolitan
                     area. I prefer to travel internationally to explore the world, and I have
-                    traveled to 40 countries so far. My most recent trip was to{' '}
-                    <b>Egypt and Jordan</b>. I speak <b>English</b> and <b>Spanish</b> fluently,
-                    with some <b>survival French</b>. I would like to learn <b>Italian</b>.
+                    traveled to 41 countries so far. My most recent trip was to <b>New Zealand</b>.
+                    I speak <b>English</b> and <b>Spanish</b> fluently, with some{' '}
+                    <b>survival French</b>. I would like to learn <b>Italian</b>.
                   </p>
                 </AboutMeSection>
                 <AboutMeSection icon="food">
