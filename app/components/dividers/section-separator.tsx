739ece095890929629338f1f134b57f162f37b3a
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2025-09-01 12:28:27 -0400

    Remove default margin on <SectionSeparator>

--- app/components/dividers/section-separator.tsx
+++ app/components/dividers/section-separator.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 
-const SectionSeparator = ({ className = 'mt-28 mb-24' }: { className?: string }) => {
+const SectionSeparator = ({ className = '' }: { className?: string }) => {
   return <hr className={classNames('border-neutral-200', className)} />;
 };
 
